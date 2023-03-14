7f7b02846dbd784681844c9625168c6d59382b8b
Blair Hall <blair.hall.og@gmail.com>
2023-03-13 22:40:06 -0400

    Exercise Created

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log(4);
+// String
+ "Hello World";
+// Boolean
+ true; 
+ false;
+// numbers
+1;
+2; 
+3;
+// Null 
+null;
+// Undefined
+undefined;
+// Operators + - * / ** %
+6 % 5;
+11 % 5;
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk"+"eye");
+let firstName = "Blair"
+let lastName = "Hall"
+let length = firstName.length + lastName.length;
+console.log(length);
+let text = "Avengers: Endgame";
+let index = text.indexOf("e");
+console.log(index);
